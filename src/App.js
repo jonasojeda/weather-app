e25f1b9835f7b2c33c490c341ad40f5176c0d99c
jonasojeda <76564885+jonasojeda@users.noreply.github.com>
2023-01-28 12:50:16 -0300

    Folder organization

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import {Route} from 'react-router-dom';
-import Home from './pages/Home/Home';
-import City from './pages/City/City';
-import About from './pages/About/About';
+
 function App() {
   return (
     <div className="App">
-      <Route exact path={"/"} component={Home} />
-      <Route exact path={"/city"} component={City} />
-      <Route exact path={"/about"} component={About} />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
